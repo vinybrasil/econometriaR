91d29ecfa1bb92d208452d613e9bf3693a171d5a
SgtMotleyCrue <vinicyusbrasil1@hotmail.com>
2019-09-04 10:42:02 -0300

    Update mortalidadeInfantil.R

--- mortalidadeInfantil.R
+++ mortalidadeInfantil.R
@@ -5,8 +5,8 @@ file <- "Table 6_4.xls"
 df <- read_excel(file, sheet="Table 6_4", col_names=TRUE, skip=2)
 #a 3 linha eh o cabeçalho, por isso nao pula ela, mas só se col name = true
 #colnames <- c("MI", "TAF", "PNB", "TFT")
-plot(df$PGNP, df$CM, pch=16, col="black")
-#plot(df$CM, df$PGNP, pch=16, col="black")
+plot(df$PGNP, df$CM, pch=16, col="blue")
+plot(df$FLR, df$CM, pch=16, col="blue") #plot(x, y)
 
 #criar o x
 
